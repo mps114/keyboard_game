0301ade2a0d61841a67d899fdd5ca2a5c819ce0c
Philipp Seitz <philippseitz@Philipps-MacBook-Pro.local>
2026-03-09 11:21:49 -0600

    Fix Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/keyboard_name/',
+  base: '/keyboard_game/',
   plugins: [react()],
 })
